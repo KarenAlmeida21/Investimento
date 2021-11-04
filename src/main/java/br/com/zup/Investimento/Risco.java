bc8879f8c6048b00950945ec6248f6f132c68d5e
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-04 11:54:26 -0300

    Criar Classe enum Risco

---
+++ src/main/java/br/com/zup/Investimento/Risco.java
@@ -0,0 +1,12 @@
+package br.com.zup.Investimento;
+
+public enum Risco {
+    BAIXO(0.0005),
+    MEDIO(0.0015),
+    ALTO(0.0025);
+    private double taxaDeRetorno;
+
+    Risco(double taxaDeRetorno) {
+        this.taxaDeRetorno = taxaDeRetorno;
+    }
+}
